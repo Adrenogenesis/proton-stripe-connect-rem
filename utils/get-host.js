cec99fee9704ce8b53a6c9f8ab7d658c771b4450
protonwebmaster <exonet3i@gmail.com>
2021-04-08 15:29:34 +0200

    pjson commit

--- utils/get-host.js
+++ utils/get-host.js
@@ -6,7 +6,7 @@ function getHost(req) {
   const {host} = req.headers;
 
   if (host.startsWith('localhost')) {
-    return `http://${host}`;
+    return `https://${host}`;
   }
   return `https://${host}`;
 }
